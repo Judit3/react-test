499fee5027b41d38435083356639f970f887478a
Judit3 3 <katapunpinpun@yahoo.es>
2024-03-07 10:03:54 +0000

    first commit

---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function FunctionalGreeting() {
+//   return <h1>Hello</h1>
+// }
+
+const FunctionalGreetingWithProps = () => <h1>Hello you</h1>
+
+export default FunctionalGreetingWithProps;
